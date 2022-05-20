b7fc62eb6fa3330b45bb8dc0b2dc1903df302830
Kirill <kir.mbr@gmail.com>
2022-05-20 21:37:12 +0300

    fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 const PingAction = require('./ping');
 const PlayAction = require('./play');
 const StopAction = require('./stop');
+const SkipAction = require('./skip');
 
 module.exports = {
   PingAction,
   PlayAction,
   StopAction,
+  SkipAction,
 };
