0539b42be190fcbd663be4933064e0905171885f
Kirill <kir.mbr@gmail.com>
2022-05-20 20:31:30 +0300

    add skip

---
+++ src/actions/skip.js
@@ -0,0 +1,34 @@
+const { getVoiceConnection, createAudioResource } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+
+const Action = require('../core/action');
+const singleton = require('../singleton');
+
+const YTDL_PRESET = { filter: 'audioonly', quality: 'highestaudio' };
+
+class SkipAction extends Action {
+  static action = 'skip';
+
+  static description = 'Play next url from queue';
+
+  /**
+   * Reply to message with pong
+   * @returns {Promise<void>}
+   */
+  async execute() {
+    const connection = getVoiceConnection(this.guildId);
+    if (!connection) {
+      this.reply('No music to play');
+      return;
+    }
+    const url = await singleton.storage.musicQueues.poll(this.guildId);
+    if (!url) {
+      this.reply('Queue is empty');
+      return;
+    }
+    const { player } = singleton.storage.players.getPlayer(this.guildId);
+    player.play(createAudioResource(ytdl(url, YTDL_PRESET), { metadata: {} }));
+  }
+}
+
+module.exports = SkipAction;
