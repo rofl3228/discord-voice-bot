03162ad0ed994c4e830ae2cf9cb6c63a0289af3b
Kirill <kir.mbr@gmail.com>
2022-06-05 23:16:01 +0300

    rm server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-const fetch = require('node-fetch');
 const config = require('config');
 
 const Bot = require('./core/bot');
-const server = require('./server');
 const singleton = require('./singleton');
 const storage = require('./stores');
 const { log } = require('./common/logger');
@@ -12,14 +10,6 @@ const bot = new Bot(config.get('bot.token'));
 
 singleton.storage.init()
   .then(() => bot.launch())
-  .then(() => {
-    server.listen(process.env.PORT, () => {
-      log.info(`Server listening on ${process.env.PORT}`);
-    });
-    setInterval(() => {
-      fetch(config.get('server.selfUrl'), { method: 'GET' }).catch((err) => log.error({ err }, 'FETCH_ERROR'));
-    }, parseInt(config.get('server.pingTimeout'), 10));
-  })
   .then(() => {
     log.info('Application started');
   })
